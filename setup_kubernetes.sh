de6687c975ed123e8c9ab4de46c56355bbca8e82
duartefilipe <filipeduarte.si@gmail.com>
2024-06-10 12:46:17 -0300

    Update setup_kubernetes.sh

--- setup_kubernetes.sh
+++ setup_kubernetes.sh
@@ -50,7 +50,7 @@ configurar_rede() {
     sudo systemctl restart containerd
 
     echo "Instalando Kubernetes..."
-    sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+    sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
     sudo echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
     sudo apt update
     sudo apt install -y kubelet kubeadm kubectl
